054b1bdf40c52f15a3b253481a11f329a81400e7
Jonathan Kim <jonnykimbk@hotmail.com>
2023-06-23 21:05:08 -0300

    movie details

--- components/movieDetail/movieDetail.vue
+++ components/movieDetail/movieDetail.vue
@@ -1,44 +1,73 @@
 <template>
-  <div :key="detail.id" class="h-screen w-full">
+  <div :key="detail.id" class="bg-slate-800 md:h-screen">
     <p>
       {{ id }}
     </p>
     <div class="md:flex flex-column pt-2" v-if="detail.title">
-      <div class="md:pr-40 md:w-2/3">
+      <div class="md:pr-40 md:w-2/3 pb-2 md:pt-2">
         <img
           class=""
           :src="'https://image.tmdb.org/t/p/w500' + detail.poster_path"
         />
       </div>
 
-      <div>
-        <h1 class="text-3xl pb-2">
+      <div class="border-2 p-2 border-sky-500">
+        <h1 class="text-3xl pb-2 font-bold">
           {{ detail.title }}
         </h1>
 
-        <div
-          v-if="
-            detail.belongs_to_collection !== null &&
-            detail.belongs_to_collection.backdrop_path !== null
-          "
-        >
-          <img
-            :src="
-              'https://image.tmdb.org/t/p/w500' +
-              detail.belongs_to_collection.backdrop_path
+        <div>
+          <div
+            v-if="
+              detail.belongs_to_collection !== null &&
+              detail.belongs_to_collection.backdrop_path !== null
             "
-          />
+          >
+            <img
+              :src="
+                'https://image.tmdb.org/t/p/w500' +
+                detail.belongs_to_collection.backdrop_path
+              "
+            />
+          </div>
+          <div v-else>
+            <img
+              :src="'https://image.tmdb.org/t/p/w500' + detail.backdrop_path"
+            />
+          </div>
         </div>
-        <div v-else>
-          <img
-            :src="'https://image.tmdb.org/t/p/w500' + detail.backdrop_path"
-          />
+
+        <div>
+          <span class="font-bold text-neutral-500"> Score: </span>
+          <span :class="color">
+            {{ Math.round(detail.vote_average * 10) }}%
+          </span>
+        </div>
+
+        <div>
+          <span>
+            <span class="font-bold text-neutral-500">Release date: </span>
+            <span>{{ detail.release_date }}</span>
+          </span>
+        </div>
+
+        <div>
+          <span>
+            <span class="font-bold text-neutral-500"> Genres: </span>
+
+            <span class="pr-3" v-for="genre in detail.genres"
+              >| {{ genre.name }} |</span
+            >
+          </span>
         </div>
 
         <div class="bg-slate-800">
-          <p class="pt-2 md:w-4/5">
-            {{ detail.overview }}
-          </p>
+          <span class="pt-2 md:w-4/5">
+            <span class="font-bold text-neutral-500"> Description: </span>
+            <p>
+              {{ detail.overview }}
+            </p>
+          </span>
         </div>
       </div>
     </div>
@@ -47,12 +76,22 @@
 
 <script>
 import axios from "axios";
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 
 export default {
   setup() {
     let { id } = useRoute().params;
     let detail = ref({});
+    let color = ref("text-red-500");
+
+    const setColor = (detail) => {
+      if (detail.vote_average >= 7) {
+        color.value = "text-green-500";
+      }
+      if (detail.vote_average < 7 && detail.vote_average >= 5) {
+        color.value = "text-yellow-400";
+      }
+    };
 
     async function fetchDetail() {
       const options = {
@@ -70,15 +109,16 @@ export default {
       );
 
       detail.value = response.data;
+
+      setColor(response.data);
     }
 
-    // onMounted(() => {
     fetchDetail();
-    // });
 
     return {
       id,
       detail,
+      color,
     };
   },
 };
